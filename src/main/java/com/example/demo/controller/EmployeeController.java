c2f9d0d6d953e1349a61bc33f45d086df5954fb3
bala-amypo <bala@amypo.in>
2025-12-18 18:20:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import com.example.demo.model.Employee;
+import com.example.demo.service.EmployeeService;
+
+@RestController
+@RequestMapping("/api/employees")
+@Tag(name = "Employee")
+public class EmployeeController {
+
+    private final EmployeeService service;
+
+    public EmployeeController(EmployeeService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Employee create(@RequestBody Employee employee) {
+        return service.createEmployee(employee);
+    }
+
+    @PutMapping("/{id}")
+    public Employee update(@PathVariable Long id, @RequestBody Employee employee) {
+        return service.updateEmployee(id, employee);
+    }
+
+    @GetMapping("/{id}")
+    public Employee get(@PathVariable Long id) {
+        return service.getEmployeeById(id);
+    }
+
+    @GetMapping
+    public List<Employee> getAll() {
+        return service.getAllEmployees();
+    }
+
+    @PutMapping("/{id}/deactivate")
+    public void deactivate(@PathVariable Long id) {
+        service.deactivateEmployee(id);
+    }
+}
