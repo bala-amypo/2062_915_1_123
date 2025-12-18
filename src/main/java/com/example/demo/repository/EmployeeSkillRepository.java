e7f685080699682a6852e742c0d002b7c6af3b84
bala-amypo <bala@amypo.in>
2025-12-18 18:05:25 +0000

    User commit

--- src/main/java/com/example/demo/repository/EmployeeSkillRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeSkillRepository.java
@@ -1,7 +1,19 @@
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.Skill;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
+import com.example.demo.model.EmployeeSkill;
+import com.example.demo.model.Employee;
 
-public interface SkillRepository extends JpaRepository<Skill, Long> {
+public interface EmployeeSkillRepository extends JpaRepository<EmployeeSkill, Long> {
+
+    @Query("SELECT e.employee FROM EmployeeSkill e WHERE e.skill.name IN :skills AND e.employee.id = :userId AND e.active = true")
+    List<Employee> findEmployeesByAllSkillNames(
+            @Param("skills") List<String> skills,
+            @Param("userId") Long userId
+    );
+
+    List<EmployeeSkill> findByEmployeeIdAndActiveTrue(Long employeeId);
+    List<EmployeeSkill> findBySkillIdAndActiveTrue(Long skillId);
 }
