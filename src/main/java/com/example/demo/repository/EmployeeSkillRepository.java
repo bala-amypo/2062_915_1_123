bbf714e72d4f1cfeb962866c240dc95c77544d53
bala-amypo <bala@amypo.in>
2025-12-19 10:42:28 +0000

    User commit

--- src/main/java/com/example/demo/repository/EmployeeSkillRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeSkillRepository.java
@@ -10,7 +10,12 @@ import java.util.List;
 
 public interface EmployeeSkillRepository extends JpaRepository<EmployeeSkill, Long> {
 
-    
+    @Query("""
+        SELECT es.employee
+        FROM EmployeeSkill es
+        WHERE es.skill.name IN :skills
+        AND es.employee.id = :userId
+    """)
     List<Employee> findEmployeesByAllSkillNames(
             @Param("skills") List<String> skills,
             @Param("userId") Long userId
