65d48f14657bed6f25ecdb864d468cb4fed0d322
bala-amypo <bala@amypo.in>
2025-12-19 06:04:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Skill.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Skill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private Boolean active = true;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public Boolean getActive() { return active; }
+    public void setActive(Boolean active) { this.active = active; }
+}
