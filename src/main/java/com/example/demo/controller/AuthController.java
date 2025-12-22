2fbd7216e868fb08d2abc07254188bd6da676492
bala-amypo <bala@amypo.in>
2025-12-22 10:29:57 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,35 +1,35 @@
-package com.example.demo.controller;
+// package com.example.demo.controller;
 
-import com.example.demo.dto.AuthRequest;
-import com.example.demo.dto.AuthResponse;
-import com.example.demo.dto.RegisterRequest;
-import com.example.demo.service.UserService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+// import com.example.demo.dto.AuthRequest;
+// import com.example.demo.dto.AuthResponse;
+// import com.example.demo.dto.RegisterRequest;
+// import com.example.demo.service.UserService;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/auth")
-public class AuthController {
+// @RestController
+// @RequestMapping("/auth")
+// public class AuthController {
 
-    private final UserService userService;
+//     private final UserService userService;
 
-    public AuthController(UserService userService) {
-        this.userService = userService;
-    }
+//     public AuthController(UserService userService) {
+//         this.userService = userService;
+//     }
 
-    @PostMapping("/register")
-    public ResponseEntity<AuthResponse> register(
-            @RequestBody RegisterRequest request) {
+//     @PostMapping("/register")
+//     public ResponseEntity<AuthResponse> register(
+//             @RequestBody RegisterRequest request) {
 
-        AuthResponse response = userService.register(request);
-        return ResponseEntity.ok(response);
-    }
+//         AuthResponse response = userService.register(request);
+//         return ResponseEntity.ok(response);
+//     }
 
-    @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(
-            @RequestBody AuthRequest request) {
+//     @PostMapping("/login")
+//     public ResponseEntity<AuthResponse> login(
+//             @RequestBody AuthRequest request) {
 
-        AuthResponse response = userService.login(request);
-        return ResponseEntity.ok(response);
-    }
-}
+//         AuthResponse response = userService.login(request);
+//         return ResponseEntity.ok(response);
+//     }
+// }
